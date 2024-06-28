2a509a9e57e40a1bc968f33dc8f8aa30cdc09378
ThatPartyboy <puma8807@gmail.com>
2024-06-29 02:53:15 +0800

    initial

--- src/@/components/ui/select.jsx
+++ src/@/components/ui/select.jsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
 import { Check, ChevronDown, ChevronUp } from "lucide-react"
 
-import { cn } from "/home/lib/utils"
+import { cn } from "home/lib/utils"
 
 const Select = SelectPrimitive.Root
 
